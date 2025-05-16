4d1a8caadb758455a139c0a450c471f50715d0dd
illiaboiko <illiaboiko.095@gmail.com>
2025-05-16 10:32:38 +0300

    tasks 6.4-6.6 done

--- redux-anecdotes/src/App.jsx
+++ redux-anecdotes/src/App.jsx
@@ -2,7 +2,7 @@ import { useSelector, useDispatch } from 'react-redux'
 import { newAnecdoteActionCreator, voteActionCreator } from './reducers/anecdoteReducer'
 
 const App = () => {
-  const anecdotes = useSelector(state => state)
+  const anecdotes = useSelector(state => state.slice().sort((a,b)=> b.votes - a.votes))
   const dispatch = useDispatch()
 
   const vote = (id) => {
